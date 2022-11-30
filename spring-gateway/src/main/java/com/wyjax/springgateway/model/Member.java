293ab2c634fcae5bc70bb1d99396608f69648256
umjugnki <umjugnki@daou.co.kr>
2022-11-30 17:40:34 +0900

    feat: test controller

---
+++ spring-gateway/src/main/java/com/wyjax/springgateway/model/Member.java
@@ -0,0 +1,16 @@
+package com.wyjax.springgateway.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class Member {
+    private String name;
+    private int age;
+
+    public Member(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
