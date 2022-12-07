693a2b3e74a2d1d9ce6adada83fbb298d33dc5bb
wyjax <wyjax.dev@gmail.com>
2022-12-07 17:41:33 +0900

    refactor: 코드 수정

--- spring-gateway/src/main/java/com/wyjax/springgateway/gateway/config/RoutingHandler.java
+++ spring-gateway/src/main/java/com/wyjax/springgateway/gateway/config/RoutingHandler.java
@@ -1,4 +1,4 @@
-package com.wyjax.springgateway.gateway;
+package com.wyjax.springgateway.gateway.config;
 
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
--- spring-gateway/src/main/java/com/wyjax/springgateway/gateway/filter/MainGlobalFilter.java
+++ spring-gateway/src/main/java/com/wyjax/springgateway/gateway/filter/MainGlobalFilter.java
@@ -14,7 +14,7 @@ public class MainGlobalFilter implements GlobalFilter, Ordered {
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         ServerHttpRequest request = exchange.getRequest();
-        log.info("global filter > path {}, uri {}", request.getPath(), request.getURI());
+        log.info("global filter > path: {}, uri: {}", request.getPath(), request.getURI());
         return chain.filter(exchange);
     }
 
