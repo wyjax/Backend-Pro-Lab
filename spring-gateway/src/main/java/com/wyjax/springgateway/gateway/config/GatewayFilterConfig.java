1233e58b586e114bf08c58ce646e7c7456994f46
wyjax <wyjax.dev@gmail.com>
2022-12-20 00:05:03 +0900

    feat: user key resolver 정의

--- spring-gateway/src/main/java/com/wyjax/springgateway/gateway/config/GatewayFilterConfig.java
+++ spring-gateway/src/main/java/com/wyjax/springgateway/gateway/config/GatewayFilterConfig.java
@@ -2,8 +2,11 @@ package com.wyjax.springgateway.gateway.config;
 
 import com.wyjax.springgateway.gateway.filter.MainGlobalFilter;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Mono;
 
 @Configuration
 public class GatewayFilterConfig {
@@ -11,5 +14,10 @@ public class GatewayFilterConfig {
     @Bean
     public GlobalFilter globalFilter() {
         return new MainGlobalFilter();
+    }
+
+    @Bean
+    KeyResolver userKeyResolver() {
+        return exchange -> Mono.just(exchange.getRequest().getQueryParams().getFirst("user"));
     }
 }
