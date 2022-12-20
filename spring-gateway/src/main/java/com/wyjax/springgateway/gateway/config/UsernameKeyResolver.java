57d6f8a2ba741dc3cb07b9985fa816840800191b
wyjax <wyjax.dev@gmail.com>
2022-12-20 14:14:14 +0900

    feat: resolver 수정

--- spring-gateway/src/main/java/com/wyjax/springgateway/gateway/config/UsernameKeyResolver.java
+++ spring-gateway/src/main/java/com/wyjax/springgateway/gateway/config/UsernameKeyResolver.java
@@ -1,13 +1,15 @@
 package com.wyjax.springgateway.gateway.config;
 
 import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
+@Component("usernameKeyResolver")
 public class UsernameKeyResolver implements KeyResolver {
     @Override
     public Mono<String> resolve(ServerWebExchange exchange) {
-        String username = exchange.getRequest().getHeaders().getFirst("X-USERNAME");
-        return Mono.just(username);
+        String name = exchange.getRequest().getQueryParams().getFirst("name");
+        return Mono.just(name);
     }
 }
