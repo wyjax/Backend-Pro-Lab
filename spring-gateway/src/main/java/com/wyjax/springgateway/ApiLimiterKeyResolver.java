dc094a30d84e6da69f2dd26b6551fa0b81b57971
wyjax <wyjax.dev@gmail.com>
2022-12-29 23:06:38 +0900

    feat: key 충돌 주석처리

--- spring-gateway/src/main/java/com/wyjax/springgateway/ApiLimiterKeyResolver.java
+++ spring-gateway/src/main/java/com/wyjax/springgateway/ApiLimiterKeyResolver.java
@@ -7,18 +7,18 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
-@Service
-@RequiredArgsConstructor
-public class ApiLimiterKeyResolver implements KeyResolver {
-    private final ApiLimiterService apiLimiterService;
-    private final ObjectHelper objectHelper;
-
-    @Override
-    public Mono<String> resolve(ServerWebExchange exchange) {
-        String path = exchange.getRequest().getPath().value();
-        String method = exchange.getRequest().getMethodValue();
-        return apiLimiterService.getApiLimiter(path, method)
-                .doOnNext(apiLimiter -> apiLimiter.setPath(path))
-                .map(objectHelper::toStringBase64);
-    }
-}
+//@Service
+//@RequiredArgsConstructor
+//public class ApiLimiterKeyResolver implements KeyResolver {
+//    private final ApiLimiterService apiLimiterService;
+//    private final ObjectHelper objectHelper;
+//
+//    @Override
+//    public Mono<String> resolve(ServerWebExchange exchange) {
+//        String path = exchange.getRequest().getPath().value();
+//        String method = exchange.getRequest().getMethodValue();
+//        return apiLimiterService.getApiLimiter(path, method)
+//                .doOnNext(apiLimiter -> apiLimiter.setPath(path))
+//                .map(objectHelper::toStringBase64);
+//    }
+//}
