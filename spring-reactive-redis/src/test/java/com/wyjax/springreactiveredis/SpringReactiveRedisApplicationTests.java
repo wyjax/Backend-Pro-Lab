7f54557acfcd8c4e8b20ebab6ecd37ccacc45544
wyjax <wyjax.dev@gmail.com>
2023-01-07 00:35:42 +0900

    feat: rective redis project init

---
+++ spring-reactive-redis/src/test/java/com/wyjax/springreactiveredis/SpringReactiveRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wyjax.springreactiveredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactiveRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
