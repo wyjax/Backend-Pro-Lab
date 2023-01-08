b13f958f65be74800658c84110a5d1df72ce9ae0
wyjax <wyjax.dev@gmail.com>
2023-01-08 20:51:22 +0900

    feat: Person 테스트 controller 추가

--- spring-reactive-redis/src/main/java/com/wyjax/springreactiveredis/controller/RedisController.java
+++ spring-reactive-redis/src/main/java/com/wyjax/springreactiveredis/controller/RedisController.java
@@ -1,26 +1,34 @@
 package com.wyjax.springreactiveredis.controller;
 
+import com.wyjax.springreactiveredis.dto.CacheModel;
+import com.wyjax.springreactiveredis.dto.request.PersonModel;
 import lombok.RequiredArgsConstructor;
-import org.springframework.cache.Cache;
-import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
 @RestController
 @RequiredArgsConstructor
 public class RedisController {
-    private static final String CACHE_NAME = "black";
-    private final RedisCacheManager redisCacheManager;
+    private final ReactiveRedisTemplate<String, Object> template;
 
     @GetMapping("/api/cache")
-    public Mono<String> getCache(@RequestParam String key) {
-        Cache.ValueWrapper valueWrapper = redisCacheManager.getCache(CACHE_NAME).get(key);
-        return Mono.just(valueWrapper.get().toString());
+    public Mono<Object> getCache(@RequestParam(value = "key") String key) {
+        return template.opsForValue().get(key);
     }
 
     @PostMapping("/api/cache")
-    public Mono<String> saveCache(@RequestBody String cache) {
-        redisCacheManager.getCache(CACHE_NAME).put(cache, cache);
-        return Mono.just("success");
+    public Mono<Boolean> saveCache(@RequestBody CacheModel model) {
+        return template.opsForValue().set(model.getKey(), model.getValue());
+    }
+
+    @GetMapping("/api/person")
+    public Mono<Object> getPerson(@RequestParam(value = "key") String key) {
+        return template.opsForValue().get(key);
+    }
+
+    @PostMapping("/api/person")
+    public Mono<Boolean> addPerson(@RequestBody PersonModel model) {
+        return template.opsForValue().set(model.getUuid(), model);
     }
 }
