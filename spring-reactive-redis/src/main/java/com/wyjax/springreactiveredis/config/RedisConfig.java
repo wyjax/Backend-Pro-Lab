8ae4ea08af1685c45ca97346c9619114e192228b
wyjax <wyjax.dev@gmail.com>
2023-01-10 23:41:26 +0900

    feat: 불필요 주석 제거

--- spring-reactive-redis/src/main/java/com/wyjax/springreactiveredis/config/RedisConfig.java
+++ spring-reactive-redis/src/main/java/com/wyjax/springreactiveredis/config/RedisConfig.java
@@ -33,42 +33,4 @@ public class RedisConfig {
                         .build();
         return new ReactiveRedisTemplate<>(factory, serializationContext);
     }
-    /*
-        GenericJackson2JsonRedisSerializer 을 value Serializer 로 사용하지 않는 이유는
-        json string으로 redis에 저장시 class 정보가 같이 들어가게 된다. 그래서 저장한 후에 필드 정보가 맞더라도
-        class 정보에 맞지 않는다면 deSerialize 시에 문제가 발생하게 된다.
-     */
-
-//
-//    @Bean
-//    public ReactiveRedisTemplate<String, String> customReactiveRedisTemplate(ReactiveRedisConnectionFactory factory) {
-//                StringRedisSerializer keySerializer = new StringRedisSerializer();
-//        Jackson2JsonRedisSerializer<String> valueSerializer = new Jackson2JsonRedisSerializer<>(String.class);
-//
-//        RedisSerializationContextBuilder<String, String> builder = newSerializationContext(keySerializer);
-//        RedisSerializationContext<String, String> context = builder.value(valueSerializer).build();
-////        return new ReactiveRedisTemplate<>(factory, context);
-//
-//
-//        return new ReactiveRedisTemplate<>(factory, RedisSerializationContext.string());
-//    }
-
-//    @Bean
-//    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
-//        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-//        redisTemplate.setConnectionFactory(factory);
-//        redisTemplate.setKeySerializer(new StringRedisSerializer());
-//        redisTemplate.setValueSerializer(new StringRedisSerializer());
-//        return redisTemplate;
-//    }
-//
-//    @Bean
-//    public ReactiveRedisTemplate<String, String> reactiveRedisTemplate(ReactiveRedisConnectionFactory factory) {
-//        StringRedisSerializer keySerializer = new StringRedisSerializer();
-//        Jackson2JsonRedisSerializer<String> valueSerializer = new Jackson2JsonRedisSerializer<>(String.class);
-//
-//        RedisSerializationContextBuilder<String, String> builder = newSerializationContext(keySerializer);
-//        RedisSerializationContext<String, String> context = builder.value(valueSerializer).build();
-//        return new ReactiveRedisTemplate<>(factory, context);
-//    }
 }
