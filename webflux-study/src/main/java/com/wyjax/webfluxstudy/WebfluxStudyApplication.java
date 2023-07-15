6196cfdca51f999abd31c7a28e30a7f5d5399c05
wyjax <wyjax.dev@gmail.com>
2023-07-15 22:50:27 +0900

    feat: webflux 스터디

---
+++ webflux-study/src/main/java/com/wyjax/webfluxstudy/WebfluxStudyApplication.java
@@ -0,0 +1,13 @@
+package com.wyjax.webfluxstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxStudyApplication.class, args);
+	}
+
+}
