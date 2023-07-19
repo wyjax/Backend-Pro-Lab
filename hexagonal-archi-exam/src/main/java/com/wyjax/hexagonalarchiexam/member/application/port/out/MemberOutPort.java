7db3886de03455a34830105e5090b670d032c0b6
wyjax <umjugnki@daou.co.kr>
2023-07-19 16:36:50 +0900

    feat: 헥사고널 아키텍처 구성연습

---
+++ hexagonal-archi-exam/src/main/java/com/wyjax/hexagonalarchiexam/member/application/port/out/MemberOutPort.java
@@ -0,0 +1,10 @@
+package com.wyjax.hexagonalarchiexam.member.application.port.out;
+
+import com.wyjax.hexagonalarchiexam.member.domain.Member;
+
+public interface MemberOutPort {
+
+    Member save(Member member);
+
+    Member get(Long memberId);
+}
