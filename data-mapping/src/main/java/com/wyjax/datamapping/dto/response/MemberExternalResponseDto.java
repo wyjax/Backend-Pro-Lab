08b9405ca39f5da8d0d9960e610287836e5e66ff
wyjax <wyjax.dev@gmail.com>
2023-07-28 14:54:40 +0900

    feat: dto mapping mapstruct

---
+++ data-mapping/src/main/java/com/wyjax/datamapping/dto/response/MemberExternalResponseDto.java
@@ -0,0 +1,12 @@
+package com.wyjax.datamapping.dto.response;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class MemberExternalResponseDto {
+
+    private String myName;
+    private String myAge;
+}
