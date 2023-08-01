150fb792bb17530d6281d4d05252cc88b8bc8050
wyjax <wyjax.dev@gmail.com>
2023-08-01 09:52:05 +0900

    feat: mapper 변경

--- data-mapping/src/main/java/com/wyjax/datamapping/mapper/MemberMapper.java
+++ data-mapping/src/main/java/com/wyjax/datamapping/mapper/MemberMapper.java
@@ -2,6 +2,7 @@ package com.wyjax.datamapping.mapper;
 
 import com.wyjax.datamapping.domain.Member;
 import com.wyjax.datamapping.dto.response.MemberExternalResponseDto;
+import com.wyjax.datamapping.dto.response.MemberInfoResponseDto;
 import com.wyjax.datamapping.dto.response.MemberResponseDto;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -14,6 +15,9 @@ public interface MemberMapper {
 
     MemberResponseDto toResponseDto(Member member);
 
-    @Mapping(target = "name", source = "myName")
+    @Mapping(target = "myName", source = "name")
+    @Mapping(target = "myAge", source = "age")
     MemberExternalResponseDto toExternalResponse(Member member);
+
+    MemberInfoResponseDto toMemberInfoResponse(Member member, String address);
 }
