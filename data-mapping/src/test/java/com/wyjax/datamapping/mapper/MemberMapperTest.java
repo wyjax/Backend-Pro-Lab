19a71a43096dbbdf9c1ee7559d09f6118ccc2aa5
wyjax <wyjax.dev@gmail.com>
2023-08-01 10:59:07 +0900

    test: given when then 주석 추가

--- data-mapping/src/test/java/com/wyjax/datamapping/mapper/MemberMapperTest.java
+++ data-mapping/src/test/java/com/wyjax/datamapping/mapper/MemberMapperTest.java
@@ -13,22 +13,28 @@ class MemberMapperTest {
 
     @Test
     void mapperSetterTest() {
+        // given
         Member member = new Member("엄정기", 29);
+        // when
         MemberResponseDto dto = MemberMapper.INSTANCE.toResponseDto(member);
         System.out.println(dto);
     }
 
     @Test
     void mapperNoSetterTest() {
+        // given
         Member member = new Member("엄정기", 29);
+        // when
         MemberExternalResponseDto dto = MemberMapper.INSTANCE.toExternalResponse(member);
         System.out.println(dto);
     }
 
     @Test
     void 여러_파라미터주입() {
+        // given
         Member member = new Member("엄정기", 29);
         String address = "경기도 용인시 기흥구";
+        // when
         MemberInfoResponseDto dto = MemberMapper.INSTANCE.toMemberInfoResponse(member, address);
         System.out.println(dto);
     }
