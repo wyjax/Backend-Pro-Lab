144135dbfa51541c6767c33f0ec2a7993a57ddf1
wyjax <wyjax.dev@gmail.com>
2023-08-02 09:15:27 +0900

    feat: hibernate study

---
+++ hibernate-study/src/main/java/com/wyjax/hibernatestudy/HibernateStudyApplication.java
@@ -0,0 +1,13 @@
+package com.wyjax.hibernatestudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateStudyApplication.class, args);
+	}
+
+}
---
+++ hibernate-study/src/test/java/com/wyjax/hibernatestudy/HibernateStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wyjax.hibernatestudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
