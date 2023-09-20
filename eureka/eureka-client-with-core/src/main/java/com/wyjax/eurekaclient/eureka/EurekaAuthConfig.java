405080eea170fba650d91a6f2b53caf9810dfa65
wyjax <wyjax.dev@gmail.com>
2023-09-20 09:48:27 +0900

    feat: eureka client-with core 작업

---
+++ eureka/eureka-client-with-core/src/main/java/com/wyjax/eurekaclient/eureka/EurekaAuthConfig.java
@@ -0,0 +1,59 @@
+package com.wyjax.eurekaclient.eureka;
+
+import com.netflix.appinfo.ApplicationInfoManager;
+import com.netflix.appinfo.EurekaInstanceConfig;
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.appinfo.MyDataCenterInstanceConfig;
+import com.netflix.appinfo.providers.EurekaConfigBasedInstanceInfoProvider;
+import com.netflix.discovery.DefaultEurekaClientConfig;
+import com.netflix.discovery.DiscoveryClient;
+import com.netflix.discovery.EurekaClient;
+import com.netflix.discovery.EurekaClientConfig;
+import com.netflix.discovery.shared.transport.jersey3.Jersey3TransportClientFactories;
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EurekaAuthConfig {
+
+    private static ApplicationInfoManager applicationInfoManager;
+    private static EurekaClient eurekaClient;
+
+    @PostConstruct
+    public void init() {
+//        DynamicPropertyFactory configInstance = DynamicPropertyFactory.getInstance();
+        MyDataCenterInstanceConfig config = new MyDataCenterInstanceConfig();
+        ApplicationInfoManager applicationInfoManager = initializeApplicationInfoManager(config);
+        EurekaClient eurekaClient = initializeEurekaClient(applicationInfoManager,
+            new DefaultEurekaClientConfig());
+
+        EurekaService eurekaService = new EurekaService(applicationInfoManager, eurekaClient);
+        try {
+            eurekaService.start();
+        } finally {
+            // the stop calls shutdown on eurekaClient
+            eurekaService.stop();
+        }
+    }
+
+    private static synchronized ApplicationInfoManager initializeApplicationInfoManager(
+        EurekaInstanceConfig instanceConfig) {
+        if (applicationInfoManager == null) {
+            InstanceInfo instanceInfo = new EurekaConfigBasedInstanceInfoProvider(
+                instanceConfig).get();
+            applicationInfoManager = new ApplicationInfoManager(instanceConfig, instanceInfo);
+        }
+
+        return applicationInfoManager;
+    }
+
+    private static synchronized EurekaClient initializeEurekaClient(
+        ApplicationInfoManager applicationInfoManager, EurekaClientConfig clientConfig) {
+        if (eurekaClient == null) {
+            eurekaClient = new DiscoveryClient(applicationInfoManager, clientConfig,
+                Jersey3TransportClientFactories.getInstance());
+        }
+
+        return eurekaClient;
+    }
+}
