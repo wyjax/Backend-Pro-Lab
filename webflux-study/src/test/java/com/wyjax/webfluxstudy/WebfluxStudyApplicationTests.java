0876030c0ef0853fcc6f08938da49bd331e6ff94
wyjax <wyjax.dev@gmail.com>
2023-10-25 14:25:19 +0900

    test: webflux flatMap에 관련하여 학습

--- webflux-study/src/test/java/com/wyjax/webfluxstudy/WebfluxStudyApplicationTests.java
+++ webflux-study/src/test/java/com/wyjax/webfluxstudy/WebfluxStudyApplicationTests.java
@@ -1,13 +1,23 @@
 package com.wyjax.webfluxstudy;
 
+import com.wyjax.webfluxstudy.study.TestExample;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import reactor.core.publisher.Flux;
 
 @SpringBootTest
 class WebfluxStudyApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    TestExample testExample = new TestExample();
 
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    public void good() {
+        // 확인하면 bad에서는 empty가 reuturn, good에서는 error signal이 방출된다
+        Flux<Integer> result = testExample.filterWhen_good();
+        System.out.println(result);
+    }
 }
